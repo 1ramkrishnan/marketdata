b73c945695c47920bb1b5f30cab953cf2df58e2d
rk <rktheaver@gmail.com>
2020-08-25 17:34:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var NSEAPI = API.NSE;
 
 var app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
